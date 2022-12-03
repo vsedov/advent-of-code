6466820d74a572d454f16c898ea6f8cdf095e689
vsedov <viv.sedov@hotmail.com>
2022-12-03 05:59:03 +0000

    Year_2022_day_03 solved.

--- tests/aoc2022/2022_day_03_test.py
+++ tests/aoc2022/2022_day_03_test.py
@@ -3,11 +3,17 @@ import pytest
 from src.aoc_cj.aoc2022 import day_03 as d
 
 TEST_INPUT = """
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
 """.strip()
 
 
 def test_a():
-    assert d.part_a(TEST_INPUT) == 0
+    assert d.part_a(TEST_INPUT) == 157
 
 
 def test_b():
