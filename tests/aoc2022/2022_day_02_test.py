f44caace49d71d2a6add52b3f47af1a525fbdf5f
vsedov <viv.sedov@hotmail.com>
2022-12-03 07:45:51 +0000

    Remove dead test case

--- tests/aoc2022/2022_day_02_test.py
+++ tests/aoc2022/2022_day_02_test.py
@@ -1,5 +1,3 @@
-import pytest
-
 from src.aoc_cj.aoc2022 import day_02 as d
 
 TEST_INPUT = """
@@ -15,12 +13,3 @@ def test_a():
 
 def test_b():
     assert d.part_b(TEST_INPUT) == 12
-
-
-@pytest.mark.skip(reason="CLI Fails")
-def test_all():
-    from aocd import get_data
-
-    data = get_data(day=2, year=2022)
-    assert d.part_a(data) == 15337
-    assert d.part_b(data) == 11696
