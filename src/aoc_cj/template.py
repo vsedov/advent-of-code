88a53dd707099f537e318e27ee775c73532c34e8
vsedov <viv.sedov@hotmail.com>
2022-12-04 07:04:20 +0000

    template for day 05 created

--- src/aoc_cj/template.py
+++ src/aoc_cj/template.py
@@ -88,4 +88,4 @@ def check_path_file(path, file) -> None:
         print(f"File {file} already exists")
 
 
-create_file(day=1, year=2021)
+create_file(day=5, year=2022)
---
+++ tests/aoc2022/2022_day_05_test.py
@@ -0,0 +1,16 @@
+
+import pytest
+
+from src.aoc_cj.aoc2022 import day_05 as d
+
+TEST_INPUT = """
+""".strip()
+
+
+def test_a() -> None:
+    assert d.part_a(TEST_INPUT) == 0
+
+
+def test_b() -> None:
+    assert d.part_b(TEST_INPUT) == 0
+
