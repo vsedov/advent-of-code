a8a21f5e94fda1d68927daaabf42933fd49fd261
vsedov <viv.sedov@hotmail.com>
2022-12-06 05:30:00 +0000

    clean up day 06

--- src/aoc_cj/aoc2022/day_06.py
+++ src/aoc_cj/aoc2022/day_06.py
@@ -2,17 +2,13 @@ from src.aoc_cj.aoc2022 import YEAR, get_day
 from src.aoc_cj.aoc_helper import Aoc
 
 
-def part_a(txt: str) -> int:
-    return parser(4, txt)
-
-
 def parser(n: int, txt: str) -> int:
     txt = txt.strip()
-    for i in range(n, len(txt)):
-        container = set(txt[i - n:i])
-        if len(container) == n:
-            return i
-    return 0
+    return [i for i in range(n, len(txt)) if len(set(txt[i - n:i])) == n][0]
+
+
+def part_a(txt: str) -> int:
+    return parser(4, txt)
 
 
 def part_b(txt: str) -> int:
