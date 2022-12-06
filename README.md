35cb68356b0967993cb7b9c2bc258cbc7c94f732
vsedov <viv.sedov@hotmail.com>
2022-12-06 06:15:33 +0000

    update readme

--- README.md
+++ README.md
@@ -10,6 +10,31 @@ Expect bad code, although I will try my best to be as _pythonic_ as possible >.<
   />
 </p>
 
+##### How to install plugin
+
+**Supports AOC Test Cases**
+Make sure to export your session id, before doing this step
+
+```
+env_location/bin/pip install advent-of-code
+
+ ➜  aoc --years 2022 --days 1 2 3 4 5 6 --u github
+```
+
+**Template**
+
+```
+python -m src -c day year
+```
+
+Will Create a test case and and the file to work on
+
+#### Features
+
+- **Pytest**
+- **AOC Test Cases**
+- **Auto templte based on day**
+
 ##### Credits and Appreciation
 
 ----
--- src/aoc_cj/__init__.py
+++ src/aoc_cj/__init__.py
@@ -4,8 +4,6 @@ import logging
 from pathlib import Path
 from typing import Literal, Optional, Union
 
-from aocd import submit
-
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 LOGS_DIR = PROJECT_ROOT / "logs"
 LOGS_DIR.mkdir(exist_ok=True)
@@ -19,48 +17,24 @@ logging.basicConfig(
 )
 
 
-def solve_part(part: Literal["a", "b"], module, data) -> Answer:
-    if f := getattr(module, f"part_{part}", None):
-        assert inspect.isfunction(f)
-
-        resp = f(data)
-        print(resp)
-        assert resp is None or isinstance(resp, (int, str))
-        return resp
-    return None
-
-
 def solve(year: int, day: int, data: str) -> tuple[Answer, Answer]:
     ans_a: Answer = None
     ans_b: Answer = None
 
     try:
-        module = importlib.import_module(f"{__name__}.aoc{year}.day_{day:02d}")
 
-        ans_a = solve_part("a", module, data)
-        ans_b = solve_part("b", module, data)
-
-        test_module = importlib.import_module(f"tests.aoc{year}.{year}_day_{day:02d}_test")
-
-        def test_part(part: Literal["a", "b"]) -> bool:
-            if f := getattr(test_module, f"test_{part}", None):
+        def solve_part(part: Literal["a", "b"]) -> Answer:
+            if f := getattr(module, f"part_{part}", None):
                 assert inspect.isfunction(f)
-                try:
-                    f()
-                    return True
-                except AssertionError:
-                    return False
-            return False
+                resp = f(data)
+                assert resp is None or isinstance(resp, (int, str))
+                return resp
+            return None
 
-        test_pass = (test_part("a"), test_part("b"))
-        print(test_pass)
+        module = importlib.import_module(f"src.aoc_cj.aoc{year}.day_{day:02d}")
 
-        if all(test_pass):
-
-            submit(ans_a, part="a", day=day, year=year)
-            submit(ans_b, part="b", day=day, year=year)
-        else:
-            logging.warning(f"Test Case is not parsing{test_pass}")
+        ans_a = solve_part("a")
+        ans_b = solve_part("b")
 
     except ModuleNotFoundError as e:
         raise NotImplementedError() from e
